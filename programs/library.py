e516869674cacd6d651f461dee65da70b690c683
Alex <alexanderesolis@gmail.com>
2021-10-05 19:50:23 -0400

    worked on the library, and added launch scripts

--- programs/library.py
+++ programs/library.py
@@ -9,32 +9,69 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 from math import *
 
 class FUNCTION_LIBRARY:
-    def __init__(self, robot, ev3, left_motor, right_motor, medium_motor, color_sensor_1, color_sensor_2):
+    def __init__(self, robot, ev3, leftDriveMotor, rightDriveMotor, leftAttachment, rightAttachment, colorSensor1, colorSensor2, gyroscope3, gyroscope4):
         #self, DriveBase, Hub
         self.driveBase = robot
         self.hub = ev3
         
-        self.left_motor = left_motor
-        self.right_motor = right_motor
-        self.medium_motor = medium_motor
+        self.leftDriveMotor = leftDriveMotor
+        self.rightDriveMotor = rightDriveMotor
+        self.leftAttachment = leftAttachment
+        self.rightAttachment = rightAttachment
 
-        self.left_motor.reset_angle(0)
-        self.left_motor.reset_angle(0)
+        self.leftDriveMotor.reset_angle(0)
+        self.rightDriveMotor.reset_angle(0)
 
         self.stopWatch = StopWatch()
-        self.color_sensor_1 = color_sensor_1 
-        self.color_sensor_2 = color_sensor_2
+
+        self.colorSensor1 = colorSensor1 
+        self.colorSensor2 = colorSensor2
+        self.gyroscope3 = gyroscope3
+        self.gyroscope4 = gyroscope4
+
+        self.black = 9
+        self.white = 85
+
+        self.gyro3Drift = False
+        self.gyro4Drift = False
 
     def shutdown(self):
         self.hub.speaker.say("Logic error, error error error error error error error error error error errorrr Non halting program detected, shutting down")
         #self.hub.speaker.say("Shutting down...")
         self.hub.speaker.play_notes(['C4/4', 'F3/4', 'F2/4'])
 
-    def line_follow_until_black(self, p=1.2, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, sensor_stop=-1,  debug=False):
+    def calibrate(self):
+        self.checkGyroscopes()
+        self.calibrateColors()
+    
+    def checkGyroscopes(self):
+        self.gyroscope3.resetAngle(0)
+        self.gyroscope4.resetAngle(0)
+        self.robot.sleep(1)
+        self.gyro3Drift = self.gyroscope3.angle != 0
+        self.gyro4Drift = self.gyroscope4.angle != 0
+
+    def callibrateColors(self):
+        numbersDone = 0
+        print("old black: " + self.black + ", old white: " + self.white)
+        while True:
+            if Button.LEFT in self.hub.buttons.pressed():
+                self.black = (self.colorSensor1.reflection() + self.colorSensor2.reflection()) / 2
+                numbersDone += 1
+
+            if Button.RIGHT in self.hub.buttons.pressed():
+                self.white = (self.colorSensor1.reflection() + self.colorSensor2.reflection()) / 2
+                numbersDone += 1
+            
+            if numbersDone == 2:
+                break
+        print("new black: " + self.black + ", new white: " + self.white)
+
+    def lineFollowUntilBlack(self, p=1.2, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, sensor_stop=-1,  debug=False):
         if (sensor_lf == -1):
-            sensor_lf = self.color_sensor_1
+            sensor_lf = self.colorSensor1
         if (sensor_stop == -1):
-            sensor_stop = self.color_sensor_2 
+            sensor_stop = self.colorSensor2 
 
         PROPORTIONAL_GAIN = p
         #BLACK = 9 #what is black
@@ -52,11 +89,11 @@ class FUNCTION_LIBRARY:
                 self.driveBase.stop()
                 break #thrillitup
 
-    def line_follow_until_white(self, p=1.2, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, sensor_stop=-1,  debug=False):
+    def lineFollowUntilWhite(self, p=1.2, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, sensor_stop=-1,  debug=False):
         if (sensor_lf == -1):
-            sensor_lf = self.color_sensor_1
+            sensor_lf = self.colorSensor1
         if (sensor_stop == -1):
-            sensor_stop = self.color_sensor_2 # I NEED TO CREATE NEW CONSTANt
+            sensor_stop = self.colorSensor2 # I NEED TO CREATE NEW CONSTANt
 
         PROPORTIONAL_GAIN = p
         #BLACK = 9 #what is black
@@ -75,9 +112,9 @@ class FUNCTION_LIBRARY:
                 break #thrillitup
 
 
-    def line_follow_for_time(self, p=1, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, time=10000, debug=False):
+    def lineFollowForTime(self, p=1, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, time=10000, debug=False):
         if (sensor_lf == -1):
-            sensor_lf = self.color_sensor_2
+            sensor_lf = self.colorSensor2
         self.stopWatch.reset()
         self.stopWatch.resume()
 
@@ -100,13 +137,13 @@ class FUNCTION_LIBRARY:
         self.driveBase.stop()
         self.hub.speaker.say("I line followed for" + str(floor(time/1000)) + "seconds")
 
-    def line_follow_for_distance(self, p=1, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, distance=10000, debug=False):
+    def lineFollowForDistance(self, p=1, DRIVE_SPEED=100, BLACK=9, WHITE= 85, sensor_lf=-1, distance=10000, debug=False):
         #BLACK = 9 #what is black
         #WHITE = 85 #what is white, also what is life (42)
         if (sensor_lf == -1):
-            sensor_lf = self.color_sensor_2
+            sensor_lf = self.colorSensor2
         elif (sensor_lf == -2):
-            sensor_lf = self.color_sensor_1
+            sensor_lf = self.colorSensor1
         PROPORTIONAL_GAIN = p
         threshold = (BLACK + WHITE) / 2 #the center of black+white
         startingDistance = self.driveBase.distance()
@@ -123,13 +160,13 @@ class FUNCTION_LIBRARY:
         self.driveBase.stop()
         ##self.hub.speaker.say("I have reached " + str(floor(distance/25.4)) + "inches") removed this, pauses robot too long
 
-    def mm_to_inch(self, mm):
+    def mmToInch(self, mm):
         return mm/25.4
-    def inch_to_mm(self, inch):
+    def inchToMM(self, inch):
         return inch*25.4
-    def ms_to_second(self, ms):
+    def msToSecond(self, ms):
         return ms/1000
-    def second_to_ms(self, s):
+    def secondToMS(self, s):
         return s*1000
 
         
