222d55ac3917952ad7fc330357ec04ee6ade5f54
holyokecodes3 <90287474+holyokecodes3@users.noreply.github.com>
2021-12-04 11:00:06 -0500

    Arjana changes 12/4

--- programs/comboThree.py
+++ programs/comboThree.py
@@ -8,23 +8,26 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import Image, SoundFile, ImageFile
 
 def comboThree(library):
+    library.leftAttachment.run_until_stalled(100)
     library.driveBase.straight(445)
     library.turn(-90)
-    library.driveBase.straight(200)
-    library.driveBase.straight(-200)
+    library.driveBase.straight(130)
+    library.driveBase.straight(-130)
     library.turn(90)
-    library.driveBase.straight(200)
-    library.turn(40)
-    library.driveBase.straight(170)
-    library.turn(35)     
-    library.leftAttachment.run_target(100, -165)
+    library.driveBase.straight(230)
+    library.turn(37)
+    library.leftAttachment.reset_angle(0)
+    library.leftAttachment.run_target(100, -260)
     library.leftAttachment.hold()
-    library.driveBase.straight(35)
-    library.leftAttachment.run_target(180, 100)
-    #library.driveBase.straight(-35)
-   # library.turn(-45)
+    library.driveBase.straight(165)
+    library.turn(25)
+    library.driveBase.straight(20)
+    library.leftAttachment.run_until_stalled(100)
+    library.driveBase.straight(-45)
+    library.turn(-25)
+    library.driveBase.straight(-20)
     library.leftAttachment.stop()
-
+    library.driveBase.stop()
 
    # library.leftAttachment.run_target(100, -100)
 
