dc7553b19479c96d0d711713589ad2eac08da677
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2021-12-07 17:29:36 -0500

    Combo four is now 95% good

--- programs/comboFour.py
+++ programs/comboFour.py
@@ -9,9 +9,9 @@ from pybricks.media.ev3dev import Image, SoundFile, ImageFile
 
 def comboFour(library):
     #Running motor until stalled to align the attachment, then stop.
-    library.rightAttachment.run_until_stalled(60)
-    library.rightAttachment.stop()
-
+    #library.rightAttachment.run_until_stalled(30)
+    #library.rightAttachment.stop()
+    library.rightAttachment.run_time(60, 62)
      #Go up to the line to line follow
     library.driveBase.straight(library.inchToMM(6.5))
     library.turn(40, 100) 
@@ -21,34 +21,36 @@ def comboFour(library):
     library.lineFollowForDistance(distance=library.inchToMM(18.25),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
     
     #Knock over first bridge
-    library.rightAttachment.run_angle(-60, 30)
+    library.rightAttachment.run_angle(-60, 35)
     library.lineFollowForDistance(distance=library.inchToMM(4.25),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
-    library.rightAttachment.run_angle(-60, 30)
+    library.rightAttachment.run_angle(-60, 35)
 
     #Knock over second bridge
     library.lineFollowForDistance(distance=library.inchToMM(7.5),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
-    library.rightAttachment.run_angle(60, 45)
+    library.rightAttachment.run_angle(60, 55)
     library.driveBase.straight(library.inchToMM(-4))
 
     #Head towards crane
-    library.lineFollowForDistance(distance=library.inchToMM(11.75),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
-    library.turn(225)
-    library.driveBase.straight(library.inchToMM(9))
-    library.turn(-15)
+    library.lineFollowForDistance(distance=library.inchToMM(11.25),DRIVE_SPEED=100,p=-.7,sensor_lf=library.colorSensor1)
+    library.turn(-115)
+    library.driveBase.straight(library.inchToMM(8.25))
+    library.turn(-12)
+
+    
 
     #Push the crane back, and begin getting ready to go to the Accident Avoidance
-    library.driveBase.straight(library.inchToMM(-14))
-    library.driveBase.straight(library.inchToMM(22))
-
+    library.driveBase.straight(library.inchToMM(-13))
+    library.driveBase.straight(library.inchToMM(20))
+    
     #Go to Accident Avoidance and knock it over.
-    library.driveBase.turn(-120)
-    library.driveBase.straight(library.inchToMM(-8.75))
+    library.driveBase.turn(-125)
+    library.driveBase.straight(library.inchToMM(-8.25))
     library.rightAttachment.stop()
     library.driveBase.stop()
-    #library.turn(20)
 """
     library.driveBase.turn(90)
     library.driveBase.straight(library.inchToMM(-20))
     library.driveBase.turn(90)
     library.driveBase.straight(library.inchToMM(3.5))
-    """
+    
+"""
