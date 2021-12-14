9ecaed5e37fc4765b362b927adade7903f30676f
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2021-12-13 21:57:55 -0500

    Jk, added extra container/innovation project to combo four
    and also trimmed combo one to remove unused code/random comments

--- programs/comboFour.py
+++ programs/comboFour.py
@@ -8,50 +8,47 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import Image, SoundFile, ImageFile
 
 def comboFour(library):
-    #Running motor until stalled to align the attachment, then stop.
-    #library.rightAttachment.run_until_stalled(30)
-    #library.rightAttachment.stop()
+    #Running motor to prevent it being too high
     library.rightAttachment.run_angle(-60, 40)
-     #Go up to the line to line follow5
+     
+    #Go up to the line to line follow
     library.driveBase.straight(library.inchToMM(6.5))
-    library.turn(45, 100) 
+    library.turn(42, 100) 
     library.driveBase.straight(library.inchToMM(8)) 
     
     #Line follow to truck, and push it
     library.lineFollowForDistance(distance=library.inchToMM(18.25),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
-    
+
     #Knock over first bridge
     library.rightAttachment.run_angle(-60, 35)
-    library.lineFollowForDistance(distance=library.inchToMM(4.25),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
+    library.lineFollowForDistance(distance=library.inchToMM(4.25),DRIVE_SPEED=120,p=-.7,sensor_lf=library.colorSensor1)
     library.rightAttachment.run_angle(-60, 35)
 
     #Knock over second bridge
-    library.lineFollowForDistance(distance=library.inchToMM(7.5),DRIVE_SPEED=120,p=-.9,sensor_lf=library.colorSensor1)
+    library.lineFollowForDistance(distance=library.inchToMM(7.5),DRIVE_SPEED=120,p=-.7,sensor_lf=library.colorSensor1)
     library.rightAttachment.run_angle(60, 55)
     library.driveBase.straight(library.inchToMM(-4))
 
-    #Head towards crane
+    #Head towards cargo connect circle
     library.lineFollowForDistance(distance=library.inchToMM(6.25),DRIVE_SPEED=100,p=-1.2,sensor_lf=library.colorSensor1)
     library.lineFollowForDistance(distance=library.inchToMM(5),DRIVE_SPEED=100,p=-.7,sensor_lf=library.colorSensor1)
-    library.turn(-125)
-    library.driveBase.straight(library.inchToMM(8.75))
-    library.turn(-20)
+    library.turn(160)
+    library.driveBase.straight(80)
+    library.leftAttachment.run_angle(60,-115)
 
-    
+    #Head towards crane
+    library.turn(80)
+    library.driveBase.straight(235)
+    library.turn(-32)
+    library.driveBase.straight(-360)
 
-    #Push the crane back, and begin getting ready to go to the Accident Avoidance
-    library.driveBase.straight(library.inchToMM(-12.75))
-    library.driveBase.straight(library.inchToMM(19))
-    
     #Go to Accident Avoidance and knock it over.
-    library.driveBase.turn(-125)
-    library.driveBase.straight(library.inchToMM(-8.6))
+    library.driveBase.straight(500)
+    library.turn(-125)
+    library.driveBase.straight(library.inchToMM(-8.75))
+
+    #stop the motors
+    library.leftAttachment.stop()
     library.rightAttachment.stop()
     library.driveBase.stop()
-"""
-    library.driveBase.turn(90)
-    library.driveBase.straight(library.inchToMM(-20))
-    library.driveBase.turn(90)
-    library.driveBase.straight(library.inchToMM(3.5))
     
-"""
--- programs/comboOne.py
+++ programs/comboOne.py
@@ -7,30 +7,6 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import Image, SoundFile, ImageFile
 
-def goToNext(library):
-    library.turn(-90)
-    library.driveBase.straight(-90)
-    library.turn(80)
-
-def goToPrev(library, times=1):
-    library.turn(-90)
-    for i in range(times):
-        library.driveBase.straight(90)
-    library.turn(100)
-
-def goToCorrectPos(library, tartgetPos):
-    if tartgetPos == 3: goToNext(library)
-    elif tartgetPos == 2: pass
-    elif tartgetPos == 1: goToPrev(library)
-
-def pickUp(library):
-    library.leftAttachment.reset_angle(0)
-    library.leftAttachment.run_target(300, 100)
-    library.driveBase.straight(100)
-    library.leftAttachment.reset_angle(0)
-    library.leftAttachment.run_target(200, -100)
-    library.driveBase.straight(-100)
-
 def comboOne(library):
     #goes back and forth fast to push three cargo containers into a black circle
     library.driveBase.settings(straight_speed=1000)
@@ -39,7 +15,3 @@ def comboOne(library):
     library.driveBase.straight(library.inchToMM(-21.5))
     library.driveBase.stop()
     library.driveBase.settings(straight_speed=100)
-    #power and the money,
-    #money and the power,
-    #day after day 
-    #hour after hour
