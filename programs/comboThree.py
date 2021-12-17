2b48b381db74fd8940386dd69c47dfdd788c6598
Alex <alexanderesolis@gmail.com>
2021-12-17 16:36:13 -0500

    added line following for the beginning and fixed some rotation errors near the engine

--- programs/comboThree.py
+++ programs/comboThree.py
@@ -13,26 +13,25 @@ def comboThree(library):
     # make sure left attachment is in the same place every time
     library.leftAttachment.run_until_stalled(speed)
     # drive to dumpster
-    library.driveBase.straight(445)
+    library.driveBase.straight(240)
+    library.lineFollowForDistance(distance = 200, sensor_lf = library.colorSensor2)
     # turn to push the dumpster off
     library.turn(-90)
     # push the dumpster off
-    library.driveBase.straight(130)
-    library.driveBase.straight(-130)
+    library.driveBase.straight(150)
+    library.driveBase.straight(-150)
     library.turn(90)
     # drive to engine
-    library.driveBase.straight(185)
-    library.turn(35)
-    # get left attachment in place 
+    library.driveBase.straight(195)
+    library.turn(40)
+    # get left attachment in place by lowering it
     library.leftAttachment.reset_angle(0)
     library.leftAttachment.run_target(speed, -260)
     library.leftAttachment.hold()
     # approach engine
     library.driveBase.straight(170)
-    library.turn(35)
-    library.driveBase.straight(70)
-    library.turn(20)
-    # library.driveBase.straight(10)
+    library.turn(30)
+    library.driveBase.straight(40)
     # flip engine
     library.leftAttachment.run_until_stalled(speed)
     # drive back to drop green cargo
@@ -55,7 +54,7 @@ def comboThree(library):
     library.driveBase.settings(straight_speed=1000)
     library.driveBase.stop()
     library.driveBase.straight(350)
-    library.rightAttachment.stop()
+    # library.rightAttachment.stop()
     library.driveBase.stop()
     library.driveBase.settings(straight_speed=1000)
 
